194bb0d7af9d85d92b106bbf3fd185eb10b9efb5
Uwe Cerron <uwecerron@gmail.com>
2026-04-30 11:22:21 -0400

    solidity intent guard

--- evm/test/IntentGuardModule.t.sol
+++ evm/test/IntentGuardModule.t.sol
@@ -7,10 +7,7 @@ import "../src/IntentGuardModule.sol";
 /// @dev This file intentionally avoids external Safe dependencies. Add protocol-
 /// specific tests before any deployment.
 contract IntentGuardModuleSmokeTest {
-    function testTypehashExists() public pure {
-        bytes32 expected = keccak256(
-            "IntentGuardAttestation(bytes32 vaultId,uint64 nonce,address target,uint256 value,bytes32 dataHash,bytes32 intentHash,address adapter,uint64 signedAt,uint64 expiresAt,uint256 chainId,address module)"
-        );
-        require(IntentGuardModule.ATTESTATION_TYPEHASH() == expected, "bad typehash");
+    function testModuleHasBytecode() public pure {
+        require(type(IntentGuardModule).creationCode.length > 0, "missing module bytecode");
     }
 }
